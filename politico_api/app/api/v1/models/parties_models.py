7a4d5783e53102bfa71e223350583f56651ed829
Ngahu <jamaalaraheem@gmail.com>
2019-02-08 13:48:29 +0300

    [Starts #163820429]Create of a new party model module.This module will contain all the relevant information associated to a political party.

---
+++ politico_api/app/api/v1/models/parties_models.py
@@ -0,0 +1,18 @@
+'''This module contains the party related data'''
+
+
+#import time
+import datetime
+
+
+
+PARTY_DB = []
+
+
+
+class PartyModel:
+    """
+    Description:Define methods  that handle all action regarding political parties.\n
+    """
+    pass
+    
