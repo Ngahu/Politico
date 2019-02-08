e7548ca8bd63aea6e1b2c4821867a426625e5f1d
Ngahu <jamaalaraheem@gmail.com>
2019-02-08 16:00:18 +0300

    [Feature #163822436]Create class methods for the office model.This methods are  check_office_exists to check if a political office does existget_all_offices to get all existing political offices and  create_political_office method to create a single political office.

--- politico_api/app/api/v1/models/office_models.py
+++ politico_api/app/api/v1/models/office_models.py
@@ -19,4 +19,44 @@ class OfficeModel:
         self.office_name = office_name
         self.office_type = office_type
         self.created_on = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+
+    
+
+    def create_political_office(self):
+        """
+        Description:Create a political office.\n
+        """
+        try:
+            new_office = dict(
+                office_id = self.office_id,
+                office_name = self.office_name,
+                office_type = self.office_type,
+                created_on = self.created_on
+            )
+            OFFICE_DB.append(new_office)
+
+            return new_office
+        
+        except Exception as error:
+            raise Exception(error)
+    
+
+    @classmethod 
+    def get_all_offices(cls):
+        """
+        Description:Return a list of all political parties.\n
+        """
+        return OFFICE_DB
+
+    @classmethod
+    def check_office_exists(cls,office_id):
+        """
+        Description:Check if a specific political office does exist.\n
+        Required Args: office_id.\n
+        """
+        for office in  OFFICE_DB:
+            if office['office_id'] = office_id:
+                return office
+        return None
+
         
